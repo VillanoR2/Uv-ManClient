c3059aa5a239bb87fe2db0d3ceae3deed1361058
José Miguel Quioz <pumas.chino99@gmail.com>
2019-11-18 12:39:31 -0600

    *Se agrego el GameService y se creo el cliente del mismo (JuegoCliente)

---
+++ UvManClient/Assets/Scripts/Clientes/JuegoCliente.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ServiceModel;
+using System.Collections.Generic;
+using LogicaDelNegocio.Modelo;
+using UnityEngine;
+using System.ServiceModel.Channels;
+
+public class JuegoCliente : MonoBehaviour, IGameServiceCallback
+{
+    public static JuegoCliente ClienteDelJuego;
+    private string DireccionIpDelServidor;
+    public GameServiceClient ServicioDeJuego;
+    public CuentaModel CuentaEnSesion;
+    private List<CuentaModel> CuentasEnSesion = new List<CuentaModel>();
+
+    private void RecuperarIpDelServidor()
+    {
+        DireccionIpDelServidor = SessionCliente.clienteDeSesion.direccionIpDelServidor;
+    }
+
+    private void Awake()
+    {
+        if (ClienteDelJuego == null)
+        {
+            ClienteDelJuego = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (ClienteDelJuego != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        SessionCliente.clienteDeSesion.ModificacionDeLaDireccion += ActualizarIpDelServidor;
+        RecuperarIpDelServidor();
+        InicializarServicioDeCuenta();
+    }
+
+    private void InicializarServicioDeCuenta()
+    {
+        ServicioDeJuego = new GameServiceClient(new InstanceContext(this), 
+            new NetTcpBinding(SecurityMode.None), new EndpointAddress("net.tcp://" + DireccionIpDelServidor + ":8292/GameService"));
+    }
+
+
+    private void ActualizarIpDelServidor()
+    {
+        RecuperarIpDelServidor();
+        InicializarServicioDeCuenta();
+    }
+
+    public void TerminarPartida()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SalaLlena()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NuevoCuentaEnLaSala(CuentaModel cuenta)
+    {
+        Debug.Log(cuenta.NombreUsuario);
+        CuentasEnSesion.Add(cuenta);
+    }
+}
