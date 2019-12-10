886e7f298cde4d2db3b30fbc1d0558f7204eabbf
Arturo Villa <villa.artu15@gmail.com>
2019-12-10 12:07:23 -0600

    Agrego documentacion de codigo

--- UvManClient/Assets/Maps/MainCamera.cs
+++ UvManClient/Assets/Maps/MainCamera.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Esta Clase funciona como un manager para la camara de la escena que siempre seguira al jugador.
+/// </summary>
 public class MainCamera : MonoBehaviour
 {
     Transform Target;
+
+    /// <summary>
+    /// El metodo Awake es un metodo predefinido por UNITY que su funcion es realizar todo lo que se encuentre dentro 
+    /// de el al iniciar un objeto en este caso la camara. Este metodo no recibe ningun paramatro y es de tipo void.
+    /// </summary>
     void Awake()
     {
         Target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+    /// <summary>
+    /// El metodo Update es un metodo predefinido por UNITY que su funcion es cargar todo lo que este dentro de el por cada
+    /// frame que pase durante la ejecucion. Es un metodo que no recibe valores y es de tipo void.
+    /// </summary>
     void Update()
     {
         transform.position = new Vector3(
--- UvManClient/Assets/Scripts/HUD/Puntuacion.cs
+++ UvManClient/Assets/Scripts/HUD/Puntuacion.cs
@@ -3,27 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// La clase Puntuacion se encarga de manejar el incremento de puntos que el jugador acumule en su partida.
+/// </summary>
 public class Puntuacion : MonoBehaviour
 {
     int PuntuacionActual = 0;
     public Text Marcador;
+
+    /// <summary>
+    /// El metodo Start es un metodo predefinido por UNITY que su funcion es realizar todo lo que se encuentre dentro 
+    /// de el al iniciar la ejecucion por primera vez del proyecto. Este metodo no recibe ningun parametro y es de tipo void.
+    /// </summary>    
     void Start()
     {
-        NotificationCenter.DefaultCenter().AddObserver(this,"IncrementarPuntos");
-        ActualizarMarcador();  
-        DontDestroyOnLoad(GameObject.Find("AlmacenPuntaje"));    
+        NotificationCenter.DefaultCenter().AddObserver(this, "IncrementarPuntos");
+        ActualizarMarcador();
+        DontDestroyOnLoad(GameObject.Find("AlmacenPuntaje"));
     }
-
-    void IncrementarPuntos(Notification Notification){
+    /// <summary>
+    /// El metodo IncremetarPuntos calcula en incremento de los puntos ganados y los actualiza en el marcador del HUD del jugador.
+    /// <paramref name="Notification">Tipo de parametro Notification<paramref/>
+    /// </summary>
+    void IncrementarPuntos(Notification Notification)
+    {
         int PuntosAIncrementar = (int)Notification.data;
         PuntuacionActual += PuntosAIncrementar;
         ActualizarMarcador();
     }
 
-    void ActualizarMarcador(){
+    /// <summary>
+    /// El metodo ActualizarMarcador solo cambia el valor del texto del objeto Marcador por la puncuacion actual del jugador.
+    /// </summary>
+    void ActualizarMarcador()
+    {
         Marcador.text = "Puntaje: " + PuntuacionActual.ToString();
         GameObject.Find("AlmacenPuntaje").GetComponent<Text>().text = PuntuacionActual.ToString();
-        
+
     }
 
 }
