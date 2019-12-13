704efd6ba31351a86ecd212c0ccfd55cc0034b9c
Arturo Villa <villa.artu15@gmail.com>
2019-12-13 00:10:22 -0600

    Actualizacion de documentacion

--- UvManClient/Assets/Maps/Objects/PickAxe/pickAxLogic.cs
+++ UvManClient/Assets/Maps/Objects/PickAxe/pickAxLogic.cs
@@ -8,6 +8,10 @@ public class pickAxLogic : MonoBehaviour
     public AudioClip Sound;
     public float volumeSound = 1f;
     
+    /// <summary>
+    /// El metodo OnTriggerEnter2D es un metodo predefinido por UNITY que su funcion es manejar la accion disparador cuando dos coliciones se encuentran.Es un metodo de tipo void
+    /// <paramref name="Collider">Es un parametro de tipo Collider2D <paramref/>
+    /// </summary>
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.tag == "Player")
